e6b9d465daf99a631128dc4894eabe51e6cb3a7f
akhilesh <akhileshpatel3089@gmail.com>
2021-04-14 14:55:32 -0400

    Initial Commit

---
+++ Akhilesh.java
@@ -0,0 +1,29 @@
+public  class Akhilesh{
+         int num1;
+     int num2;
+     int ans;
+     void get()
+     {
+           num1=35;
+           num2=65;
+
+     }
+     void sum()
+     {
+           ans=num1+num2;
+     }
+     void display()
+     {
+           System.out.println(ans);
+     }
+      public static void main(String[] arg)
+      {
+       Akhilesh p1=new Akhilesh();
+       p1.get();
+       p1.sum();
+       p1.display();
+      }
+}
+
+    
+     
